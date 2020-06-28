e0a952d6228bf9756b30acf01c0d6ed8a0b4f8a2
Kieran Cott <kmp.cott@gmail.com>
2020-06-28 21:45:12 +0100

    Updates

---
+++ precache-manifest.e2f24482e647593811c5978990a25dd7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdde3c0771f201ef67f4a6b059182b9",
+    "url": "/team-selector--react/index.html"
+  },
+  {
+    "revision": "70f208f30669000e0d11",
+    "url": "/team-selector--react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef8d780ac01979555f02",
+    "url": "/team-selector--react/static/css/main.fc000083.chunk.css"
+  },
+  {
+    "revision": "70f208f30669000e0d11",
+    "url": "/team-selector--react/static/js/2.88f8b51a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/team-selector--react/static/js/2.88f8b51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8d780ac01979555f02",
+    "url": "/team-selector--react/static/js/main.920ba876.chunk.js"
+  },
+  {
+    "revision": "ab19124b0a343725d00f",
+    "url": "/team-selector--react/static/js/runtime-main.0a5688c9.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/team-selector--react/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "781190aecb862fffe858d42b124658cc",
+    "url": "/team-selector--react/static/media/Montserrat-BoldItalic.781190ae.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/team-selector--react/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "428b2306e9c7444556058c70822d7d7c",
+    "url": "/team-selector--react/static/media/Montserrat-LightItalic.428b2306.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/team-selector--react/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "287208c81e03eaf08da630e1b04d80e8",
+    "url": "/team-selector--react/static/media/Montserrat-MediumItalic.287208c8.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/team-selector--react/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "2d3cef91fbb6377e40398891b90d29bf",
+    "url": "/team-selector--react/static/media/Montserrat-SemiBoldItalic.2d3cef91.ttf"
+  },
+  {
+    "revision": "0d9b62a03206f739cd34b2936a5929f1",
+    "url": "/team-selector--react/static/media/SourceSansPro-Bold.0d9b62a0.ttf"
+  },
+  {
+    "revision": "9950e9ec3672c65b4e64bfb5b562131a",
+    "url": "/team-selector--react/static/media/SourceSansPro-BoldItalic.9950e9ec.ttf"
+  },
+  {
+    "revision": "764c2cc5318c856dfccb29cd8529b51d",
+    "url": "/team-selector--react/static/media/SourceSansPro-Italic.764c2cc5.ttf"
+  },
+  {
+    "revision": "ee2a11b8055d665afd2ac1d818683ffe",
+    "url": "/team-selector--react/static/media/SourceSansPro-Light.ee2a11b8.ttf"
+  },
+  {
+    "revision": "eb5d8127e4158c7241a10cc05de930f6",
+    "url": "/team-selector--react/static/media/SourceSansPro-LightItalic.eb5d8127.ttf"
+  },
+  {
+    "revision": "5182da425f811908bed9f5b8c72fa44f",
+    "url": "/team-selector--react/static/media/SourceSansPro-Regular.5182da42.ttf"
+  },
+  {
+    "revision": "774c05ccae92d11b95a2ef722c9566a8",
+    "url": "/team-selector--react/static/media/SourceSansPro-SemiBold.774c05cc.ttf"
+  },
+  {
+    "revision": "0f3a2e37b81162b12c9fa35455540231",
+    "url": "/team-selector--react/static/media/SourceSansPro-SemiBoldItalic.0f3a2e37.ttf"
+  },
+  {
+    "revision": "d19bbdb4c119b7d984414179907a58d1",
+    "url": "/team-selector--react/static/media/github_logo_white.d19bbdb4.png"
+  },
+  {
+    "revision": "982704acaa6eb418b7b26d33228db710",
+    "url": "/team-selector--react/static/media/stadium.982704ac.jpg"
+  }
+]);
